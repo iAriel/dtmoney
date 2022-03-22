37ea5389ab6bfd2130525d8f7dc74dfff94276c3
iAriel <ariel@aluno.fapce.edu.br>
2022-03-22 18:06:24 -0300

    react-moddal add and created modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,17 @@ import Logo from '../../assets/logo.svg'
 
 import { Container, Content } from './styles';
 
-export function Header(){
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps){
+
     return(
         <Container>
             <Content>
                 <img src={Logo} alt="dtmoney"/>
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
             </Content>
